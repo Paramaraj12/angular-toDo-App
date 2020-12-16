6ba8b176cb72f7db5c1c5ef77b18491ff6e119fc
Paramaraj12 <paramaraj12@gmail.com>
2020-12-16 23:27:26 +0530

    3 files modified, 11 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { TodoService } from './todo.service';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
   declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  bootstrap:    [ AppComponent ],
+  providers: [TodoService]
 })
 export class AppModule { }
